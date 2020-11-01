7e46bb1cc82ec5b4ed850c4b1ddeec7c9c1e7ab4
Alpha Emmanuel <65494407+ealpha072@users.noreply.github.com>
2020-11-01 22:37:41 +0300

    changed the query selector functions

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 //DOM ELEMNTS
 const notifications = document.querySelector('.notification')
 const weatherIcon = document.querySelector('.weather-icon')
-const temp =document.querySelector('.temp-value');
-const description = document.querySelector('.temp-description');
-const location = document.querySelector('.location');
+const temp =document.querySelector('.temp-value p');
+const description = document.querySelector('.temp-description p');
+const location = document.querySelector('.location p');
 
 //
