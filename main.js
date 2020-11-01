cd179342d10bf5b11eff2310854b63e8693dad19
Alpha Emmanuel <65494407+ealpha072@users.noreply.github.com>
2020-11-01 22:30:58 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+//DOM ELEMNTS
+const notifications = document.querySelector('.notification')
+const weatherIcon = document.querySelector('.weather-icon')
+const temp =document.querySelector('.temp-value');
+const description = document.querySelector('.temp-description');
+const location = document.querySelector('.location');
+
+//
