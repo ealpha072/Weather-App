09da6b7a0510e057c4a3732d411422d3da477c1e
Alpha Emmanuel <65494407+ealpha072@users.noreply.github.com>
2020-11-03 14:14:00 +0300

    Added API request functions

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
-//DOM ELEMNTS
-const notifications = document.querySelector('.notification')
-const weatherIcon = document.querySelector('.weather-icon')
-const temp =document.querySelector('.temp-value p');
-const description = document.querySelector('.temp-description p');
-const location = document.querySelector('.location p');
 
-//
+const search = document.getElementById('search');
+const msg = document.querySelector('.message');
+const form  = document.querySelector('.form form')
+//get weather info
+
+
+const apiKey = '5ed29308b431d4ab2d33d5b101687671';
+//const url =`http://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${apiKey}`
+
+form.addEventListener('submit',e=>{
+	e.preventDefault();
+	const inputValue = document.getElementById('value').value
+	const url =`http://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${apiKey}`
+	fetch(url).then(function(response){
+		return response.json()
+	}).then(function(data){
+		console.log(data)
+	}).catch(function(){
+		msg.innerHTML = "Enter a valid city";
+	})
+})
