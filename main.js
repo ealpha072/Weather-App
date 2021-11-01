1571100bd452feba5b13e0fc81eab55877eb1868
ealpha072 <ealpha072@gmail.com>
2021-11-01 12:45:20 +0300

    Cards div

--- main.js
+++ main.js
@@ -3,21 +3,37 @@ const search = document.getElementById('search');
 const msg = document.querySelector('.error-message');
 const form  = document.querySelector('.form form');
 
-
 const apiKey = '5ed29308b431d4ab2d33d5b101687671';
 
 form.addEventListener('submit',e=>{
 	e.preventDefault();
 	const rowDiv = document.querySelector('.row')
 	const inputValue = document.getElementById('value').value
-	const url =`https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${apiKey}&units=metric`
+	const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${apiKey}&units=metric`
 	fetch(url).then(function(response){
 		return response.json()
 	}).then(function(data){
 		const {main, name, sys, weather} = data;
-		const icon =
-		 `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
+		const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
 
+		const card = 
+		`
+			<div class="card">
+				<div class="card-header">
+					<h5 class="city-name" data-name="${name},${sys.country}"><span>${name}</span></h2>
+				</div>
+				<div class="card-body">
+					${Math.round(main.temp)}<sup>°C</sup>
+					<figure>
+						<img class="city-icon" src="${icon}" alt="${weather[0]["main"]}">
+					</figure>
+				</div>
+				<div class="card-footer">
+					<figcaption><h5>${weather[0]["description"]}</h5></figcaption>
+				</div>
+			</div>
+		` 
+		/*
 		const ul = document.createElement('ul');
 		const listItem = document.createElement('li');
 			listItem.setAttribute('class','city');
@@ -36,10 +52,12 @@ form.addEventListener('submit',e=>{
 				<figcaption><h5>${weather[0]["description"]}</h5></figcaption>
 			</figure>
 		`;
+
 		listItem.innerHTML = listContent;
 		ul.appendChild(listItem);
-		colDiv.appendChild(ul);
-		rowDiv.appendChild(colDiv);	
+		colDiv.appendChild(ul);*/
+
+		rowDiv.appendChild(card);	
 		msg.innerHTML = "";
 		form.reset();
 		//input.focus();	
