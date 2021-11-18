61fc1d6b2fcb8cba782cfd3a0e8ce488ec78feea
ealpha072 <ealpha072@gmail.com>
2021-11-18 22:42:04 +0300

    Items to do later

--- main.js
+++ main.js
@@ -1,27 +1,4 @@
-//DOM ELEMENTS
-/*
-const apiKey = '5ed29308b431d4ab2d33d5b101687671';
 
-form.addEventListener('submit',e=>{
-	e.preventDefault();
-	const rowDiv = document.querySelector('.row')
-	const inputValue = document.getElementById('value').value
-	const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${apiKey}&units=metric`
-	fetch(url).then(function(response){
-		return response.json()
-	}).then(function(data){
-		const {main, name, sys, weather} = data;
-		const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
-		
-
-		rowDiv.appendChild(card);	
-		msg.innerHTML = "";
-		form.reset();
-		//input.focus();	
-	}).catch(function(){
-		msg.innerHTML = "Enter a valid city";
-	})
-})*/
 const apiKey = '5ed29308b431d4ab2d33d5b101687671';
 let resultsHolder = $('#search-results')
 
@@ -39,7 +16,7 @@ $(function(){
 			const {main, name, sys, weather} = data
 			const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`
 
-			let card = `<div class="card col-lg-2 col-md-3 col-sm-4 col-6 mb-2 ">
+			let card = `<div class="card col-lg-2 col-md-3 col-sm-4 col-6 mb-2">
 				<div class="card-title">
 					<h4 class="city-name">${name +', '+sys.country}</h4>
 				</div>
