ec675e621f4a437853ab3bde567b4a707665abd3
ealpha072 <ealpha072@gmail.com>
2021-11-18 21:30:37 +0300

    New js code

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 //DOM ELEMENTS
-const search = document.getElementById('search');
-const msg = document.querySelector('.error-message');
-const form  = document.querySelector('.form form');
-
+/*
 const apiKey = '5ed29308b431d4ab2d33d5b101687671';
 
 form.addEventListener('submit',e=>{
@@ -15,47 +12,7 @@ form.addEventListener('submit',e=>{
 	}).then(function(data){
 		const {main, name, sys, weather} = data;
 		const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
-
-		const card = 
-		`
-			<div class="card">
-				<div class="card-header">
-					<h5 class="city-name" data-name="${name},${sys.country}"><span>${name}</span></h2>
-				</div>
-				<div class="card-body">
-					${Math.round(main.temp)}<sup>°C</sup>
-					<figure>
-						<img class="city-icon" src="${icon}" alt="${weather[0]["main"]}">
-					</figure>
-				</div>
-				<div class="card-footer">
-					<figcaption><h5>${weather[0]["description"]}</h5></figcaption>
-				</div>
-			</div>
-		` 
-		/*
-		const ul = document.createElement('ul');
-		const listItem = document.createElement('li');
-			listItem.setAttribute('class','city');
-		const colDiv = document.createElement('div');
-			colDiv.setAttribute("class","col-6 col-md-2 col-sm-4 col-xs-6 weather-card")
-			//col-6 col-md-2 col-sm-4 col-xs-6
-		const listContent =
-		`	<h2 class="city-name" data-name="${name},${sys.country}">
-				<span>${name}</span>
-			</h2>
-			<div class="temp">
-				${Math.round(main.temp)}<sup>°C</sup>
-			</div>
-			<figure>
-				<img class="city-icon" src="${icon}" alt="${weather[0]["main"]}">
-				<figcaption><h5>${weather[0]["description"]}</h5></figcaption>
-			</figure>
-		`;
-
-		listItem.innerHTML = listContent;
-		ul.appendChild(listItem);
-		colDiv.appendChild(ul);*/
+		
 
 		rowDiv.appendChild(card);	
 		msg.innerHTML = "";
@@ -63,5 +20,24 @@ form.addEventListener('submit',e=>{
 		//input.focus();	
 	}).catch(function(){
 		msg.innerHTML = "Enter a valid city";
+	})
+})*/
+const apiKey = '5ed29308b431d4ab2d33d5b101687671';
+let resultsHolder = $('#search-results')
+
+
+$(function(){
+
+	$("form").submit((e)=>{
+		e.preventDefault()
+		let inputValue = $('input').val()
+		const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${apiKey}&units=metric`
+		fetch(url).then((response)=>{
+			return response.json()
+		}).then(data=>{
+			
+		}).catch(()=>{
+			$('.error-div').html()
+		})
 	})
 })
